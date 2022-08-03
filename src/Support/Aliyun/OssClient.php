70982d0a8e4d21a0af0c4667fcbebcfc9dd86196
王彩霖 <root@Bluedot-Development-Cailin.localdomain>
2022-08-03 17:03:24 +0800

    feat: OSS增加获取文件功能

--- src/Support/Aliyun/OssClient.php
+++ src/Support/Aliyun/OssClient.php
@@ -14,6 +14,11 @@ class OssClient
         return self::OSSClient()->putObject(config('composer.aliyun_oss_bucket'), $filename, $content, $option);
     }
 
+    public static function getObject($object, $option = [])
+    {
+        return self::OSSClient()->getObject(config('composer.aliyun_oss_bucket'), $object, $option);
+    }
+
     public static function OSSClient()
     {
         return self::$OSS ?: self::$OSS = new BaseOssClient(config('composer.aliyun_access_key_id'), config('composer.aliyun_access_key_secret'), config('composer.aliyun_oss_endpoint'));
