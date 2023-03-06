a0b5afa45cc380c74eaba58f1d7b4aadd0b0b2db
cailin.wang@blue-dot.cn <cailin.wang@blue-dot.cn>
2023-03-06 13:36:55 +0800

    feat(helpers): 增加脱敏函数

--- src/helpers.php
+++ src/helpers.php
@@ -1,55 +1,5 @@
 <?php
 
-if (!function_exists('get_url_json')) {
-    /**
-     * 获取url JSon数据
-     * @param $url
-     * @param $params
-     * @return mixed
-     */
-    function get_url_json($url, $params = [])
-    {
-        $url .= '?' . http_build_query($params);
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $res = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($res, true);
-    }
-}
-
-if (!function_exists('post_url_json')) {
-    /**
-     * 发起post请求
-     * @param $url
-     * @param $data
-     * @return mixed
-     */
-    function post_url_json($url, $data = null)
-    {
-        //初始化
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_POST, 1);
-        if ($data) {
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, [
-                'Content-Type: application/json; charset=utf-8',
-            ]);
-        }
-
-        $res = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($res, true);
-    }
-}
-
 if (!function_exists('html_trans_form')) {
     function html_trans_form($string)
     {
@@ -65,67 +15,36 @@ if (!function_exists('html_trans_form')) {
 }
 
 
-if (!function_exists('data_masking')) {
+if (!function_exists('desensitize')) {
     /**
      * 数据脱敏
      * @param $string 需要脱敏值
-     * @param int $start 开始
-     * @param int $length 结束
-     * @param string $re 脱敏替代符号
-     * @return bool|string
+     * @return string
      */
-    function data_masking($string, $start = 0, $length = 0, $re = '*')
+    function desensitize($str)
     {
-        if (empty($string)) {
-            return false;
+        // 判断是手机号还是邮箱
+        if (preg_match('/^1[3-9]\d{9}$/', $str)) {
+            // 手机号脱敏
+            return substr($str, 0, 3) . '****' . substr($str, 7);
+        } elseif (preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $str)) {
+            // 邮箱脱敏
+            $parts = explode('@', $str);
+            $name = $parts[0];
+            $domain = $parts[1];
+            if (strlen($name) <= 4) {
+                // 名称长度小于等于 4，全部脱敏
+                $name = '****';
+            } else {
+                // 名称长度大于 4，前 3 位显示，后面全部脱敏
+                $name = substr($name, 0, 3) . '****';
+            }
+            return $name . '@' . $domain;
         }
-
-        $strarr = [];
-        $mb_strlen = mb_strlen($string);
-
-        while ($mb_strlen) { //循环把字符串变为数组
-            $strarr[] = mb_substr($string, 0, 1, 'utf8');
-            $string = mb_substr($string, 1, $mb_strlen, 'utf8');
-            $mb_strlen = mb_strlen($string);
-        }
-
-        $strlen = count($strarr);
-        $begin = $start >= 0 ? $start : ($strlen - abs($start));
-        $end = $last = $strlen - 1;
-        if ($length > 0) {
-            $end = $begin + $length - 1;
-        } elseif ($length < 0) {
-            $end -= abs($length);
-        }
-        for ($i = $begin; $i <= $end; $i++) {
-            $strarr[$i] = $re;
-        }
-        if ($begin >= $end || $begin >= $last || $end > $last) {
-            return false;
-        }
-        return implode('', $strarr);
+        return $str;
     }
 }
 
-if (!function_exists('data_masking_email')) {
-    /**
-     * 邮箱数据脱敏
-     * @param $string 需要脱敏值
-     * @param string $re 脱敏替代符号
-     * @return bool|string
-     */
-    function data_masking_email($string, $re = '*')
-    {
-        if (empty($string)) {
-            return false;
-        }
-        if (strpos($string, '@') === false) {
-            return $string;
-        }
-        list($name, $doman) = explode('@', $string);
-        return data_masking($name, 1, 0, $re) . '@' . $doman;
-    }
-}
 if (!function_exists('file_upload_oss')) {
     /**
      * @param string $url 网络图片地址
@@ -136,7 +55,7 @@ if (!function_exists('file_upload_oss')) {
     {
         $ext = explode('.', $url);
         $dir = !empty($dir) ? $dir : 'XiumiEdit';
-        $path = $dir."/" . date('ymd') . '/';
+        $path = $dir . "/" . date('ymd') . '/';
         $name = $path . md5(rand(1000, 90000) . time()) . '.' . end($ext);
         $res = Composer\Support\Aliyun\OssClient::putObject($name, file_get_contents($url));
         return $res;
@@ -150,14 +69,14 @@ if (!function_exists('content_xiumiedit_oss')) {
      */
     function content_xiumiedit_oss(string $content)
     {
-        $html  = preg_match_all(' /htt(ps|p):\/\/(statics|img).xiumi.us([^"]*?)(.jpg|.JPG|.png|.PNG|.bmp|.BMP|.gif|.GIF|.jpeg|.JPEG|.webp|.WEBP|.svg|.SVG)/',$content,$LIST);
-        foreach ($LIST[0] as $k =>$value){
+        $html  = preg_match_all(' /htt(ps|p):\/\/(statics|img).xiumi.us([^"]*?)(.jpg|.JPG|.png|.PNG|.bmp|.BMP|.gif|.GIF|.jpeg|.JPEG|.webp|.WEBP|.svg|.SVG)/', $content, $LIST);
+        foreach ($LIST[0] as $k => $value) {
             $ext = explode('.', $value);
             $dir = !empty($dir) ? $dir : 'XiumiEdit';
-            $path = $dir."/" . date('ymd') . '/';
+            $path = $dir . "/" . date('ymd') . '/';
             $name = $path . md5(rand(1000, 90000) . time()) . '.' . end($ext);
             $new  = Composer\Support\Aliyun\OssClient::putObject($name, file_get_contents($value));
-            $content = str_replace($value,$new['info']['url'],$content);
+            $content = str_replace($value, $new['info']['url'], $content);
         }
         return $content;
     }
