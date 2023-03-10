9bdd2a0adabe82abaf07229864d801d16fc488c3
cailin.wang@blue-dot.cn <cailin.wang@blue-dot.cn>
2023-03-10 10:57:28 +0800

    fix(WeChat): 修复微信素材操作

--- src/Application/WeChat/MaterialClient.php
+++ src/Application/WeChat/MaterialClient.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace Composer\Application\WeChat;
+namespace App\Application\Tenant\WeChat;
 
-use Composer\Application\WeChat\Models\Authorizer;
-use Composer\Application\WeChat\Models\Material;
 use Composer\Http\Controller;
 use Spatie\QueryBuilder\AllowedFilter;
+use Composer\Application\WeChat\WeChat;
+use Composer\Application\WeChat\Models\Material;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
 
 class MaterialClient extends Controller
 {
-    public $weChat;
-
-    public function __construct(WeChat $weChat, Material $material)
+    public function __construct(Material $material)
     {
-        $this->weChat = $weChat;
         $this->model = $material;
         $this->allowedFilters = [
             AllowedFilter::exact('type'),
@@ -21,39 +21,68 @@ class MaterialClient extends Controller
         ];
     }
 
-    public function getAsync(Authorizer $authorizer)
+    public function getWeChatList(Request $request, WeChat $weChat)
     {
-        $type = request()->input('filter.type', 1);
-        $authorizerData = $authorizer::where([
-            'type' => 1,
-        ])->get();
+        $input = $request->validate([
+            'appid' => 'required',
+            'type' => Rule::in('image', 'video', 'voice', 'article'),
+            'current' => 'required',
+            'pageSize' => 'required',
+        ]);
+        $api = $weChat->getOfficialAccount($input['appid'])->getClient();
 
-        foreach ($authorizerData as $key => $value) {
-            $start = 0;
-            $limit = 10;
-            $count = 0;
-
-            $app = $this->weChat->getOfficialAccount($value['appid']);
-
-            do {
-                $list = $app->material->list('news', $start, $limit);
-                $count = $list['total_count'];
-                foreach ($list['item'] as $k => $v) {
-                    $time = date('Y-m-d H:i:s', $v['update_time']);
-                    $data = [
-                        'name' => $v['media_id'],
-                        'appid' => $value['appid'],
-                        'type' => $type,
-                        'data' => $v['content'],
-                        'created_at' => $time,
-                        'updated_at' => $time,
-                    ];
-                    $this->model->updateOrCreate(['name' => $v['media_id']], $data);
-                }
-                $start += $limit;
-            } while ($count > ($start + $limit));
+        $offset = ($input['current'] - 1) * $input['pageSize'];
+        $options = [
+            'offset' => $offset,
+            'count' => $input['pageSize'],
+        ];
+        if ($input['type'] == 'article') {
+            $options['no_content'] = 1;
+            $response = $api->postJson('/cgi-bin/freepublish/batchget', $options)->toArray();
+        } else {
+            $options['type'] = $input['type'];
+            $response = $api->postJson('/cgi-bin/material/batchget_material', $options)->toArray();
         }
 
-        return $this->success();
+        $list = [
+            'total' =>  $response['total_count'],
+            'data' =>  $response['item'],
+            'pageSize' => $input['pageSize'],
+            'current' => $input['current'],
+        ];
+
+        return $this->success($list);
+    }
+
+    public function uploadWeChat(Request $request, WeChat $weChat)
+    {
+        $input = $request->validate([
+            'appid' => 'required',
+            'type' => Rule::in('image', 'video', 'voice'),
+            'file' =>  'file',
+        ]);
+
+        $api = $weChat->getOfficialAccount($input['appid'])->getClient();
+
+        $fileName = $input['file']->storeAs('', $input['file']->getClientOriginalName(), 'uploads');
+        $filePath = Storage::disk('uploads')->path($fileName);
+
+        $result = [];
+        if ($input['type'] == 'image') {
+            $result = $api->withFile($filePath, 'media')->post('/cgi-bin/material/add_material?type=' . $input['type']);
+        }
+        Storage::disk('uploads')->delete($fileName);
+
+        return $this->success($result);
+    }
+
+    public function getWeChat($mediaId, Request $request, WeChat $weChat)
+    {
+        $input = $request->validate([
+            'appid' => 'required',
+        ]);
+        $api = $weChat->getOfficialAccount($input['appid'])->getClient();
+        $response = $api->postJson('/cgi-bin/material/get_material', ['media_id' => $mediaId]);
+        return $this->success($response->toArray());
     }
 }
