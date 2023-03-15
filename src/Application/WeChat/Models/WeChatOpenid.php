b6480f9f5ff9ea42ec3c9f633150952d75728c72
cailin.wang@blue-dot.cn <cailin.wang@blue-dot.cn>
2023-03-15 13:27:10 +0800

    feat(DevOps): 增加Env

--- src/Application/WeChat/Models/WeChatOpenid.php
+++ src/Application/WeChat/Models/WeChatOpenid.php
@@ -2,7 +2,6 @@
 
 namespace Composer\Application\WeChat\Models;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
