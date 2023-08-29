5b1c88b74f344a39f9940a9e3054902c69300d42
dongbo.sang <15225140805@163.com>
2023-08-29 14:58:07 +0800

    feat(oss):删除oss文件

--- src/Support/Aliyun/OssServerClient.php
+++ src/Support/Aliyun/OssServerClient.php
@@ -18,6 +18,11 @@ class OssServerClient
         return self::OSSClient()->getObject(config('composer.aliyun_oss_bucket'), $object, $option);
     }
 
+    public static function deleteObject($object)
+    {
+        return self::OSSClient()->deleteObject(config('composer.aliyun_oss_bucket'), $object);
+    }
+
     public static function OSSClient()
     {
         if (self::$OSS) return self::$OSS;
